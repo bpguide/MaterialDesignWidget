e4d0ab1d537e6d504ecd569376dfc744b004db51
wpp <443484837@qq.com>
2017-06-22 17:47:38 +0800

    修改DrawerLayout

--- app/src/main/java/com/wpp/study/MainActivity.java
+++ app/src/main/java/com/wpp/study/MainActivity.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 
 import com.wpp.study.recyclerview.RecyclerViewActivity;
+import com.wpp.study.side.SideActivity;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -19,7 +19,7 @@ import butterknife.Unbinder;
  * @date 2017/6/19
  */
 public class MainActivity extends AppCompatActivity {
-    Button recyclerview;
+
     private Unbinder bind;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.recyclerview_bt)
     public void startRecycerView(){
         Intent intent = new Intent(this, RecyclerViewActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.side_bt)
+    public void startSide(){
+        Intent intent = new Intent(this, SideActivity.class);
         startActivity(intent);
     }
     @Override
--- app/src/main/java/com/wpp/study/side/SideActivity.java
+++ app/src/main/java/com/wpp/study/side/SideActivity.java
@@ -2,32 +2,40 @@ package com.wpp.study.side;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.wpp.study.R;
 
-public class SideActivity extends ActionBarActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
-    private DrawerLayout drawerLayout;
+public class SideActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawerlayout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    Unbinder bind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_side);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        bind = ButterKnife.bind(this);
         setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         toggle.syncState();
-//        drawerLayout.addDrawerListener(toggle);
+        drawerLayout.addDrawerListener(toggle);
         drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
                 View menu = drawerView;
                 View content = drawerLayout.getChildAt(0);
-//                menu.setScaleX();
+                //在这里可以做一些动画效果
             }
 
             @Override
